003398060191f9268fd1312c16c073195a9e2767
spicey93 <spicerdean@icloud.com>
2025-08-17 09:04:27 +0100

    controller created for vrm

--- models/tyre.model.js
+++ models/tyre.model.js
@@ -1,7 +1,19 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const tyreSchema = new Schema({});
+const tyreSchema = new Schema({
+    brand: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    models: [
+        {
+            type: String,
+            unique: true,
+        }
+    ]
+});
 
 const Tyre = mongoose.model("Tyre", tyreSchema);
 
